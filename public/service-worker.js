481c408d4279f461e0857476327d5591eef21708
Vergota1417 <mj23mgomez3@yahoo.com>
2026-08-21 10:25:12 -0500

    Refresh PWA cache for stock search update

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='signalforge-shell-v2';
+const CACHE_NAME='signalforge-shell-v3';
 const APP_SHELL=['/','/index.html','/styles.css','/pwa.css','/config.js','/app.js','/pwa.js','/manifest.webmanifest','/icons/signalforge-icon.svg','/icons/signalforge-maskable.svg'];
 
 self.addEventListener('install',event=>{
