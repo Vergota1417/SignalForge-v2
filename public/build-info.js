26d91679cbdb2355ce34c55ec04605272b8e11f8
Vergota1417 <mj23mgomez3@yahoo.com>
2026-08-23 23:02:32 -0500

    Advance build metadata for backend self-test

--- public/build-info.js
+++ public/build-info.js
@@ -1,1 +1,1 @@
-window.SIGNALFORGE_BUILD=Object.freeze({version:'2.30.3',release:'backend-operations-visibility',shell:'v30-3'});
+window.SIGNALFORGE_BUILD=Object.freeze({version:'2.30.4',release:'backend-self-test',shell:'v30-4'});
