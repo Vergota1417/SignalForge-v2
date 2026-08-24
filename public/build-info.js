3ef8113d29d36f57abe2e13c45696538d3c1a18a
Vergota1417 <mj23mgomez3@yahoo.com>
2026-08-24 08:44:20 -0500

    Bump SignalForge opening pipeline release to v2.30.5

--- public/build-info.js
+++ public/build-info.js
@@ -1,1 +1,1 @@
-window.SIGNALFORGE_BUILD=Object.freeze({version:'2.30.4',release:'backend-self-test',shell:'v30-4'});
+window.SIGNALFORGE_BUILD=Object.freeze({version:'2.30.5',release:'opening-market-pipeline',shell:'v30-5'});
