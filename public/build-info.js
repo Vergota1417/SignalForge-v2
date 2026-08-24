88ae14488e073d2192d70aaf9ee77b6d82a9763a
Vergota1417 <mj23mgomez3@yahoo.com>
2026-08-24 13:44:46 -0500

    Stage 14.17: fix mobile simple view and stale PWA shell (#61)
    * Stage 14.17: hide duplicate advanced panels in mobile simple view
    
    * Stage 14.17: auto-activate newest PWA shell
    
    * Stage 14.17: add mobile simple-mode shell regression
    
    * Stage 14.17: bump visible build to v2.30.18
    
    * Stage 14.17: wire mobile shell regression and bump package version

--- public/build-info.js
+++ public/build-info.js
@@ -1,1 +1,1 @@
-window.SIGNALFORGE_BUILD=Object.freeze({version:'2.30.17',release:'mobile-decision-first',shell:'v30-17'});
+window.SIGNALFORGE_BUILD=Object.freeze({version:'2.30.18',release:'mobile-simple-shell-fix',shell:'v30-18'});
