ef58211ae94107edb779c958001a25a178e3d5fe
Vergota1417 <mj23mgomez3@yahoo.com>
2026-08-23 22:40:26 -0500

    Advance build metadata for backend operations visibility

--- public/build-info.js
+++ public/build-info.js
@@ -1,1 +1,1 @@
-window.SIGNALFORGE_BUILD=Object.freeze({version:'2.30.2',release:'production-visibility',shell:'v30'});
+window.SIGNALFORGE_BUILD=Object.freeze({version:'2.30.3',release:'backend-operations-visibility',shell:'v30-3'});
