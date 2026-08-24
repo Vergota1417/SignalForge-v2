5f0f6e12c375bc49f4b40e4c7230df97cea49701
Vergota1417 <mj23mgomez3@yahoo.com>
2026-08-24 09:27:44 -0500

    Bump permanent discovery retirement release to v2.30.7

--- public/build-info.js
+++ public/build-info.js
@@ -1,1 +1,1 @@
-window.SIGNALFORGE_BUILD=Object.freeze({version:'2.30.6',release:'discovery-quarantine',shell:'v30-6'});
+window.SIGNALFORGE_BUILD=Object.freeze({version:'2.30.7',release:'catalog-hardening',shell:'v30-7'});
