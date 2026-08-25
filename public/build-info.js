96465889eac9df1a4b820d1537ad70ffe652db57
Vergota1417 <mj23mgomez3@yahoo.com>
2026-08-24 21:35:39 -0500

    Bump adaptive scheduler release badge

--- public/build-info.js
+++ public/build-info.js
@@ -1,1 +1,1 @@
-window.SIGNALFORGE_BUILD=Object.freeze({version:'2.30.19',release:'alert-deeplink-summary-anchor',shell:'v30-19'});
+window.SIGNALFORGE_BUILD=Object.freeze({version:'2.30.20',release:'adaptive-api-scheduler',shell:'v30-20'});
