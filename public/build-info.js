e43ca06ec2ba739da725b2e6ce3335e6ebacdb23
Vergota1417 <mj23mgomez3@yahoo.com>
2026-08-25 11:16:42 -0500

    Stage 14.34: bump stable pattern context release

--- public/build-info.js
+++ public/build-info.js
@@ -1,1 +1,1 @@
-window.SIGNALFORGE_BUILD=Object.freeze({version:'2.30.33',release:'resource-usage-reduction',shell:'v30-33'});
+window.SIGNALFORGE_BUILD=Object.freeze({version:'2.30.34',release:'stable-pattern-context',shell:'v30-34'});
