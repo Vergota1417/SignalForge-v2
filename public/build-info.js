66aaf55ce7e8e14a1c41e1f91202e957567ff4cc
Vergota1417 <mj23mgomez3@yahoo.com>
2026-08-31 11:50:36 -0500

    Bump SignalForge to 2.30.49

--- public/build-info.js
+++ public/build-info.js
@@ -1,1 +1,1 @@
-window.SIGNALFORGE_BUILD=Object.freeze({version:'2.30.48',release:'workspace-chart-studio',shell:'v30-48'});
+window.SIGNALFORGE_BUILD=Object.freeze({version:'2.30.49',release:'twelve-data-minute-guard',shell:'v30-49'});
