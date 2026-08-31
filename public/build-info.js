50b50cd9a8fd846f62bc70566c511a5e7effa9b1
Vergota1417 <mj23mgomez3@yahoo.com>
2026-08-31 12:55:27 -0500

    Stage 16.12: mark dashboard blueprint release

--- public/build-info.js
+++ public/build-info.js
@@ -1,1 +1,1 @@
-window.SIGNALFORGE_BUILD=Object.freeze({version:'2.30.50',release:'visual-dashboard-workspace',shell:'v30-50'});
+window.SIGNALFORGE_BUILD=Object.freeze({version:'2.30.51',release:'dashboard-zero-blueprint',shell:'v30-51'});
